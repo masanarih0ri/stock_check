5f2cc8264a22976dc7f57a3040d53d593c95e8d5
masanarih0ri <masanarihoridevelop@gmail.com>
2020-09-22 17:08:07 +0900

    不要なコードを削除

--- pages/index.vue
+++ pages/index.vue
@@ -17,9 +17,6 @@ export default {
       stocksInfo: null
     }
   },
-  components: {
-    // Logo
-  },
   mounted() {
     axios
     .get(`http://localhost:3000/api/tiingo/daily/fsly/prices?token=${process.env.API_KEY}`)
@@ -29,34 +26,4 @@ export default {
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
